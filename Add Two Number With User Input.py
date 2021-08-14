68894d41b4b4cefcae80d058331e0d621d997058
Jagdish Prajapat <jprajapat888@gmail.com>
2021-08-14 12:08:26 +0530

    Subtraction Added in the Code

--- Add Two Number With User Input.py
+++ Add Two Number With User Input.py
@@ -4,6 +4,8 @@ num2 = input('Enter the second number: ')
 
 #Add two numbers
 sum = float(num1) + float(num2)
+subtraction = float(num1) + float(num2)
 
 # Display the sum
 print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+print('The subtraction of {0} and {1} is {2}'.format(num1, num2, sum))
